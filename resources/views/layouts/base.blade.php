4fb5400f3f41f97529679f09951cc188f3c80cb4
Alexander <777not888@mail.ru>
2018-11-26 15:44:50 +0300

    template added

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+	<meta charset="UTF-8">
+	<title>Froggle</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="kywords" content="">
+	<meta name="description" content="">
+	<meta name="author" content="parapix.ru">
+
+	<meta property="og:site_name" content="Froggle"/>
+	<meta property="og:title" content="Froggle - сайт скидок" />
+	<meta property="og:type" content="website"/> 
+	<meta property="og:url" content="http://froggle.com"/>
+	<meta property="og:image" content="/theme/images/icon/share.png" />
+	<meta property="og:description" content="Описание сайта" />
+
+	<link rel="apple-touch-icon" sizes="60x60" href="img/icon/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/icon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/icon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/icon/apple-touch-icon-152x152.png">
+	
+	<link rel="shortcut icon" href="img/icon/favicon.ico" type="image/x-icon">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/main.min.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.theme.css">
+</head>
+<body>
+	<noscript>
+	    <div class="notice notice--warning">
+	        <div class="container text-center">
+	            <!--noindex-->
+	            Внимание! В вашем браузере отключен <b>JavaScript</b>. Для работы с сайтом, <b>включите его</b>.
+	            <!--/noindex-->
+	        </div>
+	    </div>
+	</noscript>
+	<header class="header anim js-header">
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row-fluid">
+
+					<!-- Hamburger -->
+					<div class="col-sm-1 col-xs-1 js-search-hide">
+						<div class="menu-small anim">
+							<div class="menu-small__btn js-menu js-box-show" data-box-show="menu">
+								<span></span>
+							</div>
+						</div>
+					</div>
+					<div class="box-list">
+						<ul class="menu__list hidden js-box-list" data-box-list="menu">
+
+						</ul>
+					</div>
+					<!-- / Hamburger -->
+
+					<!-- LOGO -->
+					<div class="col-lg-3 col-md-4 col-sm-5 col-xs-10 js-search-hide">
+						<h1 class="logo anim">
+							<a role="link" href="index.html" class="logo__link">
+								<img src="img/logo.png" alt="Froggle" class="logo_img">
+							</a>
+						</h1>
+					</div>
+					<!-- / LOGO -->
+
+					<!-- Город full -->
+					<div class="col-md-offset-1 col-sm-4 col-lg-5 hidden-xs">
+						<div class="choice-city anim">
+							<div class="choice-city__link" data-toggle="modal" data-target="#modal-city-list">
+								<i class="choice-city__ico pull-left ico ico-point"></i>
+								<span class="choice-city__item">Саратов</span>
+							</div>
+						</div>
+						<ul class="choice-city__list hidden-xs hidden">
+							<li class="choice-city__list--item"><a href="" class="choice-city__list--link">Саратов</a></li>
+							<li class="choice-city__list--item"><a href="" class="choice-city__list--link">Москва</a></li>
+							<li class="choice-city__list--item"><a href="" class="choice-city__list--link">Кызыл-Юрт</a></li>
+						</ul>
+					</div>
+					<!-- / Город full -->
+
+					<div class="box-list">
+						<ul class="choice-city__list js-box-list" data-box-list="city">
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Москва</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Санкт-Петербург</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Александров (Владимирская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Архангельск (Архангельская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Астрахань (Астраханская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Белгород (Белгородская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Боровск (Калужская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Брянск (Брянская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Валдай (Новгородская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Великий Новгород (Новгородская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Владимир (Владимирская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Волгоград (Волгоградская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Вологда (Вологодская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Воронеж (Воронежская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Выборг (Ленинградская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Вязьма (Смоленская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Геленджик (Краснодарский край)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Городец (Нижегородская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Дмитров (Московская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Дубна (Московская область)</a></li>
+							<li class="choice-city__list--item"><a href="#" class="choice-city__list--link">Екатеринбург (Свердловская область)</a></li>
+						</ul>
+					</div>
+
+
+					<div class="col-lg-10 col-md-10 col-sm-10 hidden-xs pull-right search-form-big anim js-search-form">
+						<form action="" class="search-form" role="search">
+							<div class="input-group">
+							  <input type="text" class="search-form__input form-control anim" placeholder="Поиск">
+							  <span class="input-group-btn">
+							    <button role="button" class="btn btn-link search-form__submit anim" type="button"><i class="ico ico-search"></i></button>
+							  </span>
+							</div>
+						</form>
+					</div>
+
+					<div class="category-list col-xs-4 hidden-xs hidden-sm">
+						<div class="menu-category">
+							<a role="link" href="/" class="btn menu-category__link js-box-show" data-box-show="category">
+								Все категории <span class="ico ico-arrow-down-light"></span>
+							</a>
+						</div>
+					</div>
+
+					<div class="top-icon__list js-top-icon">
+						<!-- Город icon -->
+						<div class="top-icon__item hidden-sm hidden-md hidden-lg pull-right js-search-show">
+							<div class="choice-city js-box-show" data-box-show="city">
+								<a role="link" href="/" class="choice-city__link" title="">
+									<i class="choice-city__ico pull-left ico ico-point"></i>
+								</a>
+							</div>
+						</div>
+						<!-- / Город icon -->
+
+						<!-- Список icon -->
+						<div class="top-icon__item category-list-ico hidden-md hidden-lg anim js-search-hide">
+							<a role="link" href="" class="search-form__link js-box-show anim"  data-box-show="category">
+								<i class="ico ico-category-light"></i>
+							</a>
+						</div>
+
+
+						<!-- / Список icon -->
+
+						<!-- Поиск icon -->
+						<div class="top-icon__item pull-right hidden-sm hidden-md hidden-lg">
+							<span class="search-form__link link-is-active js-search-show">
+								<i class="ico ico-search-light"></i>
+							</span>
+						</div>
+						<!-- / Поиск icon -->
+					</div>
+
+					<div class="top-icon__close js-box-hide">
+						<span class="search-form__link link-is-active">
+							<i class="ico ico-close-light"></i>
+						</span>
+					</div>
+
+				</div>
+
+				<div class="row-fluid">
+					<div class="box-list">
+						<div class="category-box col-md-24 anim js-box-list" data-box-list="category">
+						<nav class="menu anim" role="navigation">
+							<ul class="list-inline list-unstyled menu__list">
+								<li class="menu__item">
+									<input id="cat1" class="menu__input" type="checkbox">
+									<label for="cat1"><i class="ico ico-arrow pull-left"></i>Развлечения</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat2" class="menu__input" type="checkbox" checked>
+									<label for="cat2"><i class="ico ico-arrow pull-left"></i>Кафе/Рестораны</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat3" class="menu__input" type="checkbox">
+									<label for="cat3"><i class="ico ico-arrow pull-left"></i>Одежда</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat4" class="menu__input" type="checkbox">
+									<label for="cat4"><i class="ico ico-arrow pull-left"></i>Авто</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat5" class="menu__input" type="checkbox">
+									<label for="cat5"><i class="ico ico-arrow pull-left"></i>Электроника</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat6" class="menu__input" type="checkbox">
+									<label for="cat6"><i class="ico ico-arrow pull-left"></i>Здоровье</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat7" class="menu__input" type="checkbox">
+									<label for="cat7"><i class="ico ico-arrow pull-left"></i>Для дома</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat8" class="menu__input" type="checkbox">
+									<label for="cat8"><i class="ico ico-arrow pull-left"></i>Строительство</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat9" class="menu__input" type="checkbox">
+									<label for="cat9"><i class="ico ico-arrow pull-left"></i>Услуги</label>
+								</li>
+								<li class="menu__item">
+									<input id="cat10" class="menu__input" type="checkbox">
+									<label for="cat10"><i class="ico ico-arrow pull-left"></i>Туризм</label>
+								</li>
+							</ul>
+						</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+    <main class="main" role="main">
+        <div class="wrapper">
+            <div class="container-fluid">
+            
+            @section ('content')
+            
+            @show
+			
+            </div>
+        </div>
+    </main>
+
+
+
+    <footer class="footer">
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-24 col-sm-12">
+						<span class="footer__copy">ООО «Рога и Копыта». © 2015 год.</span>
+					</div>
+					<div class="col-xs-24 col-sm-12">
+						<span class="footer__author">
+							Дизайн сайта: 
+							<a role="link" 
+								href="http://parapix.ru/"
+								title="Веб-студия полного цикла Parapix"
+								class="footer__link" 
+								rel="nofollow" 
+								target="blank">
+								<div class="footer__parapix"></div>
+							</a>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Modal -->
+	<div class="modal fade bs-example-modal-sm" id="modal-city-list" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-sm">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	        <h4 class="modal-title" id="myModalLabel">Ваш город:</h4>
+	      </div>
+	      <div class="modal-body text-center">
+	      	<div class="form-group">
+	      		<input type="text" class="form-control">
+	      	</div>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-primary btn-lg">Выбрать</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!-- / END Modal -->
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script>window.jQuery || document.write('jquery-1.11.3.min.js');</script>
+	<script src="js/plugins.min.js"></script>
+	<script src="js/main.min.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/p_brands.blade.php
@@ -0,0 +1,196 @@
+@extends ('layouts.base')
+           
+@section ('content')
+
+			<div class="container-fluid">
+				<div class="row">
+
+					<aside class="sidebar col-xs-24 col-md-4">
+						<div class="sidebar__wrap">
+							<div class="sidebar__item sidebar-nav">
+								<ul class="sidebar-nav__list">
+									<li class="sidebar-nav__item">
+										<a href="p_brands.html" class="sidebar-nav__link active">Бренды</a>
+									</li>
+									<li class="sidebar-nav__item">
+										<a href="p_offers.html" class="sidebar-nav__link">Акции</a>
+									</li>
+									<li class="sidebar-nav__item">
+										<a href="p_billing.html" class="sidebar-nav__link">Биллинг</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</aside>
+					<div class="report col-xs-24 col-md-20">
+							<h2 class="report__heading">Список брендов</h2>
+							<div class="row">
+								<div class="report__wrap clearfix">
+									<div class="col-xs-24 col-sm-12">
+										<div class="report__btn-wrap">
+											<a href="p_add.html" title="Добавить" class="btn btn-primary">
+												<i class="report__ico ico ico-plus"></i>
+												Добавить бренд
+											</a>
+										</div>
+									</div>
+									<div class="col-xs-24 col-sm-12">
+										<form action="" class="report__search">
+											<label class="report__search-text text-left" for="small-search">Поиск:</label>
+											<input id="small-search" class="form-control report__search-input" type="text" placeholder="Название или ID операции">
+										</form>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-xs-24">
+									<div class="table-block">
+											<div class="table-block__row hidden-xs">
+												<div class="table-block__col-head">
+													<a href="?sortby=ID&act=ASC" title="По возрастанию">
+														<i class="ico ico-arrow-sort-up"></i>
+														<i class="ico ico-arrow-sort-down hidden"></i>
+														ID
+													</a>
+												</div>
+												<div class="table-block__col-head">
+													<a href="?sortby=name&act=ASC" title="По алфавиту">
+														<i class="ico ico-arrow-sort-up hidden"></i>
+														<i class="ico ico-arrow-sort-down-active"></i>
+														Название бренда
+													</a>
+												</div>
+												<div class="table-block__col-head">
+													<a href="?sortby=ID&act=ASC" title="По возрастанию">
+														<i class="ico ico-arrow-sort-up"></i>
+														<i class="ico ico-arrow-sort-down hidden"></i>
+														Дата создания
+													</a>
+												</div>
+												<div class="table-block__col-head">
+													<a href="?sortby=ID&act=ASC" title="По возрастанию">
+														<i class="ico ico-arrow-sort-up"></i>
+														<i class="ico ico-arrow-sort-down hidden"></i>
+														Дата изменения
+													</a>
+												</div>
+												<div class="table-block__col-head"></div>
+											</div>
+											<div class="table-block__row">
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													423
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													Рубль Бум
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													16.07.2015 01:01:46
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													16.09.2015 09:32:59
+												</div>
+												<div class="table-block__col">
+													<a href="?id=423&act=edit" title="Редактировать" class="report__link--ico">
+														<i class="report__ico--edit ico ico-pencil"></i>
+													</a>
+													<a href="?id=423&act=del" titke="Удалить" class="report__link--ico">
+														<i class="report__ico--delete ico ico-close"></i>
+													</a>
+												</div>
+											</div>
+											<div class="table-block__row">
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													423
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													Рубль Бум
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													16.07.2015 01:01:46
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													16.09.2015 09:32:59
+												</div>
+												<div class="table-block__col">
+													<a href="?id=423&act=edit" title="Редактировать" class="report__link--ico">
+														<i class="report__ico--edit ico ico-pencil"></i>
+													</a>
+													<a href="?id=423&act=del" titke="Удалить" class="report__link--ico">
+														<i class="report__ico--delete ico ico-close"></i>
+													</a>
+												</div>
+											</div>
+											<div class="table-block__row">
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													423
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													Рубль Бум
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													16.07.2015 01:01:46
+												</div>
+												<div class="table-block__col">
+													<span class="cell__name"></span>
+													16.09.2015 09:32:59
+												</div>
+												<div class="table-block__col">
+													<a href="?id=423&act=edit" title="Редактировать" class="report__link--ico">
+														<i class="report__ico--edit ico ico-pencil"></i>
+													</a>
+													<a href="?id=423&act=del" titke="Удалить" class="report__link--ico">
+														<i class="report__ico--delete ico ico-close"></i>
+													</a>
+												</div>
+											</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="report__wrap clearfix">
+									<div class="col-sm-12 hidden-xs">
+										<div class="report__shows-record">Записи с 1 по 15 из 22 записей</div>
+									</div>
+									<div class="col-xs-24 col-sm-12">
+										<div class="pagination">
+											<ul class="pagination__list pagination__list--right">
+												<li class="pagination__item">
+													<a href="/" class="pagination__link">
+														<i class="ico ico-arrow-left"></i>
+													</a>
+												</li>
+												<li class="pagination__item">
+													<a href="/" class="pagination__link active">1</a>
+												</li>
+												<li class="pagination__item">
+													<a href="/" class="pagination__link">2</a>
+												</li>
+												<li class="pagination__item">
+													<a href="/" class="pagination__link">3</a>
+												</li>
+												<li class="pagination__item">
+													<a href="/" class="pagination__link">
+														<i class="ico ico-arrow-right"></i>
+													</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+
+							</div>
+					</div>
+				</div>
+				</div>
+@endsection
